02bfef03766d2b61bf58bcb8ebcc62b397c58fd2
zourui <zourui@mrray.cn>
2018-03-16 11:44:04 +0800

    my_app架构

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <div class="box"></div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    .box{
+        width: 300px;
+        height: 400px;
+        background-color: #e4393c;
+        margin: auto;
+    }
+</style>
